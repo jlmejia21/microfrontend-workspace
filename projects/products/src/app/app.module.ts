26e107849ff750b122def6d2a1cb149f3979de89
Jose Luis Mejia Rojas <joseluismejiarojas@gmail.com>
2022-02-06 16:22:02 -0500

    change index html

--- projects/products/src/app/app.module.ts
+++ projects/products/src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ProductsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private injector: Injector,
+  ) { }
+
+  ngDoBootstrap() {
+    const productscomponent = createCustomElement(ProductsComponent, { injector: this.injector });
+    customElements.define('app-products', productscomponent);
+  }
+}
